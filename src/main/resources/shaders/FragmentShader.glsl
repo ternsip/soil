a8bd7b2ec46bd740a5e92feb70e8c7f41b7de0bb
ternsip <ternsip@gmail.com>
2020-10-10 18:42:13 +0300

    Add carcass #15;

--- src/main/resources/shaders/FragmentShader.glsl
+++ src/main/resources/shaders/FragmentShader.glsl
@@ -32,24 +32,6 @@ out vec4 out_Color;
 
 uniform sampler2DArray[MAX_SAMPLERS] samplers;
 
-vec4 getTexture(int index, vec3 pos) {
-    if (index == 0) return texture(samplers[0], pos);
-    else if (index == 1) return texture(samplers[1], pos);
-    else if (index == 2) return texture(samplers[2], pos);
-    else if (index == 3) return texture(samplers[3], pos);
-    else if (index == 4) return texture(samplers[4], pos);
-    else if (index == 5) return texture(samplers[5], pos);
-    else if (index == 6) return texture(samplers[6], pos);
-    else if (index == 7) return texture(samplers[7], pos);
-    else if (index == 8) return texture(samplers[8], pos);
-    else if (index == 9) return texture(samplers[9], pos);
-    else if (index == 10) return texture(samplers[10], pos);
-    else if (index == 11) return texture(samplers[11], pos);
-    else if (index == 12) return texture(samplers[12], pos);
-    else if (index == 14) return texture(samplers[14], pos);
-    return texture(samplers[15], pos);
-}
-
 int roundFloat(float value) {
     return int(round(value));
 }
@@ -58,6 +40,6 @@ void main(void) {
 
     Quad quad = quadData[roundFloat(quadIndex)];
     TextureData textureData = textures[quad.type];
-    out_Color = getTexture(textureData.atlasNumber, vec3(texture_xy * vec2(textureData.maxU, textureData.maxV), textureData.layer));
+    out_Color = texture(samplers[textureData.atlasNumber], vec3(texture_xy * vec2(textureData.maxU, textureData.maxV), textureData.layer));
 
 }
