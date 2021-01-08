098eb1dc9c61d050aa546a4901978d951a5d4300
ternsip <ternsip@gmail.com>
2021-01-08 19:21:07 +0300

    One more command to free callbacks;

--- src/main/java/com/ternsip/soil/graph/display/WindowData.java
+++ src/main/java/com/ternsip/soil/graph/display/WindowData.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL.createCapabilities;
 import static org.lwjgl.opengl.GL11.*;
@@ -155,6 +156,7 @@ public class WindowData {
         for (Callback callback : callbacks) {
             callback.free();
         }
+        glfwFreeCallbacks(window);
         glfwTerminate();
     }
 
